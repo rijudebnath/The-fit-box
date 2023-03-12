d8928a8deaac3e945a52c299dafaa255096e5d85
Riju Debnath <debnathriju907@gmail.com>
2023-03-12 08:10:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+console.log("Riju")
+
+// const sectionHeroEl = document.querySelector(".nav");
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//    const ent = entries[0]
+//    console.log(ent)
+//    if(ent.isIntersecting === false){
+//     document.body.classList.add("sticky")
+//    }else{
+//     document.body.classList.remove("sticky")
+//    }
+//   },
+//   {
+//     root: null,
+//     threshold:0,
+//     rootMargin:"-80px"
+//   }
+// );
+// obs.observe(sectionHeroEl);
